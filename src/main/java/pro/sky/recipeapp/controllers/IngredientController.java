3575edb6f98362a8bee0a44fe365de98c2ed13f5
GrantDit <kiruha5854@mail.ru>
2023-04-02 03:55:18 +0300

    5 HomeWork

--- src/main/java/pro/sky/recipeapp/controllers/IngredientController.java
+++ src/main/java/pro/sky/recipeapp/controllers/IngredientController.java
@@ -1,5 +1,14 @@
 package pro.sky.recipeapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.recipeapp.model.Ingredient;
@@ -9,20 +18,68 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/ingredient")
+@Tag(name = "Ingredients", description = "CRUD-операции и работы с ингридиентами")
 public class IngredientController {
     private final IngredientService ingredientService;
 
     public IngredientController(IngredientService ingredientService) {
         this.ingredientService = ingredientService;
     }
-
     @PostMapping
+    @Operation(
+            summary = "Создание нового ингредиента",
+            description = "Создание нового ингредиента с его номером"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент создан",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
         Ingredient createdIngredient = ingredientService.addIngredient(ingredient);
         return ResponseEntity.ok(createdIngredient);
     }
 
     @GetMapping("/{ingredientNumber}")
+    @Operation(
+            summary = "Найти ингредиент по его номеру",
+            description = "Поиск по номеру ингредиента"
+    )
+    @Parameters(value = {
+            @Parameter(name = "ingredientNumber", example = "1")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент успешно найден",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Ингредиент не найдены",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<Ingredient> getIngredient(@PathVariable long ingredientNumber) {
         Ingredient ingredient = ingredientService.getIngredient(ingredientNumber);
         if (ingredient == null) {
@@ -30,7 +87,36 @@ public class IngredientController {
         }
         return ResponseEntity.ok(ingredient);
     }
+
     @GetMapping
+    @Operation(
+            summary = "Найти все ингредиенты",
+            description = "Найти все ингредиенты"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиенты успешно найдены",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Ингредиенты не найдены",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<Map<Long, Ingredient>> getAllIngredients() {
         Map<Long, Ingredient> ingredients = ingredientService.getAllIngredients();
         if (ingredients == null) {
@@ -38,8 +124,38 @@ public class IngredientController {
         }
         return ResponseEntity.ok(ingredients);
     }
-
-    @PutMapping("/{ingredientNumber}")
+    @PutMapping("{ingredientNumber}")
+    @Operation(
+            summary = "Обновить ингредиент по его номеру",
+            description = "Поиск по номеру ингредиента для его обновления"
+    )
+    @Parameters(value = {
+            @Parameter(name = "ingredientNumber", example = "1")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент был успешно обновлен",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Ингредиент не найден",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema =
+                                    @Schema(implementation = Ingredient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<Ingredient> editIngredient(@PathVariable long ingredientNumber, @RequestBody Ingredient ingredient) {
         Ingredient ingredient1 = ingredientService.editIngredient(ingredientNumber, ingredient);
         if (ingredient1 == null) {
@@ -48,7 +164,24 @@ public class IngredientController {
         return ResponseEntity.ok(ingredient1);
     }
 
-    @DeleteMapping("/{ingredientNumber}")
+    @DeleteMapping("ingredientNumber")
+    @Operation(
+            summary = "Удалить ингредиент по его количеству",
+            description = "Поиск по номеру ингредиента, чтобы удалить его"
+    )
+    @Parameters(value = {
+            @Parameter(name = "ingredientNumber", example = "1")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент был успешно удален"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Ингредиент не найден"
+            )
+    })
     public ResponseEntity<Void> deleteIngredientById(@PathVariable long ingredientNumber) {
         if (ingredientService.deleteIngredientById(ingredientNumber)) {
             return ResponseEntity.ok().build();
@@ -57,6 +190,17 @@ public class IngredientController {
     }
 
     @DeleteMapping
+    @Operation(
+            summary = "Удалить все ингредиенты",
+            description = "Удалить все ингредиенты"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиенты были успешно удалены"
+            )
+    })
+
     public ResponseEntity<Void> deleteAllIngredients() {
         ingredientService.deleteAllIngredients();
         return ResponseEntity.ok().build();
